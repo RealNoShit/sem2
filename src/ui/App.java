aa414f03bbc17ad2668b43dea0a52df5d04c3376
RealNoShit <ctwk2001@gmail.com>
2025-12-14 11:40:52 +0100

    Create App.java

---
+++ src/ui/App.java
@@ -0,0 +1,31 @@
+package ui;
+import ctrl.*;
+import db.*;
+import ui.*;
+import model.*;
+
+import java.sql.Connection;
+
+public class App {
+    public static void main(String[] args) {
+        // Data layer
+        Connection connection = DBConnection.getConnection();
+
+        CaseDAOIF caseDAO = new CaseDAO(connection);
+        ProductDAOIF productDAO = new ProductDAO(connection);
+        ServiceDAOIF serviceDAO = new ServiceDAO(connection);
+        OrderDAO orderDAO = new OrderDAO(connection);
+
+        // Controller layer
+        CaseController caseController = new CaseController(caseDAO);
+        ProductController productController = new ProductController(productDAO);
+        ServiceController serviceController = new ServiceController(serviceDAO);
+
+        OrderController orderController =
+                new OrderController(caseController, orderDAO, productController, serviceController);
+
+        // Presentation layer
+        OrderUI ui = new OrderUI(orderController);
+        ui.run();
+    }
+}
