5571076b149418ce8fbc9a5de3c59fb4cebd1f20
RealNoShit <ctwk2001@gmail.com>
2025-12-15 12:58:44 +0100

    Update App.java

--- src/ui/App.java
+++ src/ui/App.java
@@ -1,22 +1,32 @@
 package ui;
-import ctrl.*;
-import db.*;
-import model.*;
+
+import ctrl.CaseController;
+import ctrl.OrderController;
+import ctrl.ProductController;
+import ctrl.ServiceController;
+
+import db.CaseDAO;
+import db.CaseDAOIF;
+import db.DBConnection;
+import db.OrderDAO;
+import db.ProductDAO;
+import db.ProductDAOIF;
+import db.ServiceDAO;
+import db.ServiceDAOIF;
 
 import java.sql.Connection;
 
 public class App {
     public static void main(String[] args) {
-        // Data layer
+
         Connection connection = DBConnection.getConnection();
 
         CaseDAOIF caseDAO = new CaseDAO(connection);
         ProductDAOIF productDAO = new ProductDAO(connection);
         ServiceDAOIF serviceDAO = new ServiceDAO(connection);
-        
-        OrderDAO orderDAO = new OrderDAO(connection, caseDAO, productDAO, serviceDAO);
 
-        // Controller layer
+        OrderDAO orderDAO = new OrderDAO(connection);
+
         CaseController caseController = new CaseController(caseDAO);
         ProductController productController = new ProductController(productDAO);
         ServiceController serviceController = new ServiceController(serviceDAO);
@@ -24,8 +34,7 @@ public class App {
         OrderController orderController =
                 new OrderController(caseController, orderDAO, productController, serviceController);
 
-        // Presentation layer
-        OrderUI ui = new OrderUI(orderController);
-        ui.run();
+        OrderUI userInterface = new OrderUI(orderController);
+        userInterface.run();
     }
 }
