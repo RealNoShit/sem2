5eae9546ec4940f107584dff14556073f6c79f5f
RealNoShit <ctwk2001@gmail.com>
2025-12-15 12:58:30 +0100

    Update ProductController.java

--- src/ctrl/ProductController.java
+++ src/ctrl/ProductController.java
@@ -1,32 +1,23 @@
 package ctrl;
 
 import java.util.List;
-import java.sql.SQLException;
+
 import db.ProductDAOIF;
 import model.Product;
-import exceptions.DataAccessException; 
 
 public class ProductController {
 
-    private ProductDAOIF productDAO;
+    private final ProductDAOIF productDAO;
 
     public ProductController(ProductDAOIF productDAO) {
         this.productDAO = productDAO;
     }
 
     public Product findProductByID(int productID) {
-        try {
-            return productDAO.findProductByID(productID);
-        } catch (SQLException e) {
-            throw new DataAccessException( + productID, e);
-        }
+        return productDAO.findProductByID(productID);
     }
 
     public List<Product> findAllProducts() {
-        try {
-            return productDAO.findAllProducts();
-        } catch (SQLException e) {
-            throw new DataAccessException(, e);
-        }
+        return productDAO.findAllProducts();
     }
 }
